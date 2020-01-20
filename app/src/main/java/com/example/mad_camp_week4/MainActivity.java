c77da84d4fb42b72defa35fa6e8713500020ccaf
HyunjoonCho <jhj09016@gmail.com>
2020-01-20 22:14:44 +0900

    image fixed

--- app/src/main/java/com/example/mad_camp_week4/MainActivity.java
+++ app/src/main/java/com/example/mad_camp_week4/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> lstResultRowId = new ArrayList<>(); // 선택되지 않은 아이템을 삭제하기 위한 rowId를 저장하는 리스트
     private ArrayList<String> lstDeleteRowId = new ArrayList<>(); // 일괄 선택된 애들을 모두 지움
     private ImageView cup_of_coffee;
+    private ColorStateList oldColor;
 
     private ImageButton setting_btn, add_btn;
     private GoodsDatabase goodsDatabase = new GoodsDatabase(); // GoodsItem을 조작하기 위한 객체 생성
@@ -89,7 +91,32 @@ public class MainActivity extends AppCompatActivity {
                 TodayCoffeeAdapter todayCoffeeAdapter = new TodayCoffeeAdapter(goodsList);
                 todayCoffeeView.setAdapter(todayCoffeeAdapter);
                 TextView caffeineTextView = dialogView.findViewById(R.id.caffeine_content_edit);
+                TextView caffeineView = dialogView.findViewById(R.id.caffeine);
+                TextView caffeinemgView = dialogView.findViewById(R.id.caffeine_mg);
+                if(oldColor == null){
+                    oldColor = caffeineTextView.getTextColors();
+                }
+                if(caffeine>400){
+                    caffeineView.setTextColor(Color.RED);
+                    caffeinemgView.setTextColor(Color.RED);
+                    caffeineTextView.setTextColor(Color.RED);
+                } else {
+                    caffeineView.setTextColor(oldColor);
+                    caffeinemgView.setTextColor(oldColor);
+                    caffeineTextView.setTextColor(oldColor);
+                }
                 TextView priceTextView = dialogView.findViewById(R.id.price_edit);
+                TextView priceView = dialogView.findViewById(R.id.price);
+                TextView priceWonView = dialogView.findViewById(R.id.price_won);
+                if(price>20000){
+                    priceView.setTextColor(Color.RED);
+                    priceWonView.setTextColor(Color.RED);
+                    priceTextView.setTextColor(Color.RED);
+                } else {
+                    priceView.setTextColor(oldColor);
+                    priceWonView.setTextColor(oldColor);
+                    priceTextView.setTextColor(oldColor);
+                }
                 caffeineTextView.setText("" + caffeine);
                 priceTextView.setText("" + price);
 
